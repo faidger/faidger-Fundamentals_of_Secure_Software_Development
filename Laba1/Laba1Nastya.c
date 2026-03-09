6245df0443bca70d2687df5d2b1771627e040cb1
faidger <tarfaidger@gmail.com>
2026-03-09 17:15:52 +0600

    Add files via upload

---
+++ Laba1/Laba1Nastya.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int per() {
+    printf("Программа 3 ещё не добавлена!\n");
+    return 0;
+}
---
+++ Laba1/Laba1Vasya.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int ctr() {
+    printf("Программа 3 ещё не добавлена!\n");
+    return 0;
+}
